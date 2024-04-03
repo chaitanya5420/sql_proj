90a91c838e1a36c01378ff2ac2c9e6243e433edc
chaitanya <110461756+chaitanya5420@users.noreply.github.com>
2024-04-03 18:27:01 +0530

    login_signup added

--- settings/webapp_table/urls.py
+++ settings/webapp_table/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('', home, name='home'),
     path('students/', students, name='students'),
     path('faculty/', faculty, name = 'faculty'),
     path('library/', books, name = 'books' ),
-    path('courses/', course_item, name = 'courses')
+    path('courses/', course_item, name = 'courses'),
+    path('registration/', register, name = 'register'),
+    path('login/', custom_login, name='login'),
 ]
--- settings/webapp_table/views.py
+++ settings/webapp_table/views.py
@@ -1,11 +1,18 @@
 # views.py
 from django.shortcuts import render
 from .models import *
+from django.shortcuts import render, redirect
+from .forms import *
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
+
+def home(request):
+    return render(request, 'home.html')
 
 def students(request):
     students = Student.objects.all()
     return render(request, 'students.html', {'students': students})
-
 
 def faculty(request):
     faculty = faculties.objects.all()  # Retrieve all faculty members
@@ -18,3 +25,46 @@ def books(request):
 def course_item(request):
     courses = course.objects.all()
     return render(request,'courses.html',{'courses':courses})
+
+
+def custom_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirect to home page after login
+            else:
+                # Authentication failed
+                error_message = "Invalid username or password."
+                return render(request, 'login.html', {'form': form, 'error_message': error_message})
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def  register(request):
+    page='register'
+    
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            # Create a new user
+            user = User.objects.create_user(
+                username=username,
+                email=email,
+                password=password
+                )
+            
+            user.save()
+            return redirect('login')  # Redirect to login page after successful registration
+    else:
+        form = RegistrationForm()
+        context = {'form': form, 'page':page}
+    return render(request, 'register.html', context)
