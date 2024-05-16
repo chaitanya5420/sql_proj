ebcf10a4e48f0bad5c7a0face3b0c059a08e624d
chaitanya <110461756+chaitanya5420@users.noreply.github.com>
2024-05-16 10:13:08 +0530

    file-uploader updated

--- settings/uploader/views.py
+++ settings/uploader/views.py
@@ -8,10 +8,10 @@ import pandas as pd
 def gallary(request):
     
     category = request.GET.get('category')
-    if category ==None:
+    if category == None:                # it checks if there is data in category model if there is no data then it will show all photos
         photos=Photo.objects.all()
-    else:
-        photos=Photo.objects.filter(category__name=category)
+    else:                                   
+        photos=Photo.objects.filter(category__name=category)    #if there is data in category model then it will filterout that data on the name of category
     
     categories = Category.objects.all()
     context = {'categories':categories,'photos':photos}
@@ -25,10 +25,11 @@ def add(request):
         data= request.POST
         image =request.FILES.get('image') 
          
-        if  data['category'] != 'none':
-            category = Category.objects.get(id = data['category'])
+        if  data['category'] != 'none':                                #in it if there is data in category then it will fatch it using it's id
+            category = Category.objects.get(id = data['category']) 
         elif data['new_category'] != '':
-            category, created = Category.objects.get_or_create(name= data['new_category'])
+            category, created = Category.objects.get_or_create(name= data['new_category']) # it will check model category and the data in it if there id data then fatch it
+                                                                                           #  if not then create new data in this case it will create new category
         else:
             category = None
            
@@ -54,12 +55,15 @@ def delete_photo(request, pk):
     photos = Photo.objects.get(id=pk)
     if request.method == 'POST':
         photos.delete()
-        return redirect('gallary')  # Redirect to some other page after deletion
-    return redirect('photo', pk=pk)  # Redirect back to the view page if not a POST request
+        return redirect('gallary')     # Redirect to some home page after deletion
+    return redirect('photo', pk=pk)    # Redirect back to the view page if not a POST request
 
 
 def base(request):
-    return render(request,'base.html')
+    form = FileUploadForm()
+    files = UploadedFile.objects.all()
+    context={'form':form,'files':files}
+    return render(request,'base.html',context)
 
 
 def upload_file(request):
@@ -67,22 +71,23 @@ def upload_file(request):
         form = FileUploadForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('view')
+            return redirect('base')
     else:
         form = FileUploadForm()
-    return render(request, 'file.html', {'form': form,})
+        files = UploadedFile.objects.all()
+    return render(request, 'base.html', {'form': form,'files':files})
 
 
-def view_files(request):
-    files = UploadedFile.objects.all()
-    return render(request, 'base.html', {'files': files})
+# def view_files(request):
+#     files = UploadedFile.objects.all()
+#     return render(request, 'base.html', {'files': files})
 
 
 def show_tables(request, file_id):
     file_obj = UploadedFile.objects.get(id=file_id)
     try:
         df = pd.read_csv(file_obj.file)
-        tables = df.values.tolist() 
+        tables = df.values.tolist()  # it converts each row of datafram into a list of lists means every index value has a list as it's value
         headings = df.columns.tolist()
         context = {'file_name': file_obj.name, 'tables': tables,'headings':headings}
         return render(request, 'table.html',context )
